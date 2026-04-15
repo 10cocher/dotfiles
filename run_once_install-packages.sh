efa234668d8e777467457967cffbaa3627a69bf8
Emmanuel Cocher <10cocher.emmanuel@gmail.com>
2026-04-15 16:10:17 +0200

    [FIX] Alacritty install

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -50,7 +50,7 @@ if ! command -v alacritty &> /dev/null; then
     sudo cp target/release/alacritty /usr/local/bin/
 
     # Set up the desktop icon so it appears in your GNOME launcher
-    sudo cp extra/logo/alacritty-term+default.svg /usr/share/pixmaps/Alacritty.svg
+    sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
     sudo desktop-file-install extra/linux/Alacritty.desktop
     sudo update-desktop-database
 
