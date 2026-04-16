afe7b26672d13bc3057ddf2f7ae14250ffb5d6fe
Emmanuel Cocher <10cocher.emmanuel@gmail.com>
2026-04-16 14:14:43 +0200

    [LINT] Fix typos in echo commands

--- run_once_90_configure_systemd.sh
+++ run_once_90_configure_systemd.sh
@@ -4,5 +4,5 @@ set -e
 if command -v emacs &> /dev/null; then
     echo "Handling Emacs over to the systemd engine..."
     systemctl --user enable --now emacs
-    echo "Emacs background daemin is permanently online."
+    echo "Emacs background daemon is permanently online."
 fi
