6c2f0f727a9cf4460691c27757120b7abb4c4190
Emmanuel Cocher <10cocher.emmanuel@gmail.com>
2026-04-16 15:28:29 +0200

    [FEAT] Add zsh to install_core-dependencies.sh

--- run_once_00_install_core-dependencies.sh
+++ run_once_00_install_core-dependencies.sh
@@ -2,7 +2,7 @@
 set -e
 
 sudo apt update
-sudo apt install -y build-essential wget unzip ripgrep fd-find
+sudo apt install -y build-essential wget unzip ripgrep fd-find zsh
 
 
 # ==========================================
