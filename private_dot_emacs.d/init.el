281043cb1f54be39057dc149c0d9f71601e588e3
Emmanuel Cocher <10cocher.emmanuel@gmail.com>
2026-04-17 12:10:51 +0200

    [FEAT] Make emacs open in full-screen

--- private_dot_emacs.d/init.el
+++ private_dot_emacs.d/init.el
@@ -24,6 +24,7 @@
 (tool-bar-mode -1)                  ;; Disable the toolbar
 (tooltip-mode -1)                   ;; Disable tooltips
 (menu-bar-mode -1)                  ;; Disable the menu bar
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
 
 ;; line/column numbers
 (setq column-number-mode t)         ;; Always show column numbers
